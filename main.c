79edbea4273ca359128d2d2861ea0cf27c5e6855
LambisElef <lambiselef@gmail.com>
2020-07-26 12:21:24 +0300

    Issue with consumers not finishing when jobs were lost fixed.

--- main.c
+++ main.c
@@ -15,17 +15,18 @@
 #include "queue.h"
 #include "timer.h"
 
-#define QUEUE_SIZE 10
-#define SECONDS_TO_RUN 10
+#define QUEUE_SIZE 1
+#define SECONDS_TO_RUN 2
 
-void *work(void *arg);
-void *error();
 void *producer(void *arg);
 void *consumer(void *arg);
+void *work(void *arg);
+void *error();
 void saveT(int N, FILE *file, int *data);
 
 typedef struct {
     Queue *queue;
+    int *quit;
     int totalJobs;
     int *tJobWait;
     int *tJobOut;
@@ -34,13 +35,16 @@ typedef struct {
 } ConsumerArguments;
 
 // Counts total lost jobs due to full queue.
-int jobsLost;
+int jobsLostCounter;
 
 // Counts the number of jobs that the producers have added.
 int jobsInCounter;
 
 // Counts the number of jobs that the consumers have consumed.
 int jobsOutCounter;
+
+// Flag for the consumers to quit when needed.
+volatile int quit;
 
 int main () {
 
@@ -94,14 +98,27 @@ int main () {
     // Initializes random number seed.
     srand(time(NULL));
 
-    for (int conNum=2; conNum<5; conNum*=2) {
+    for (int conNum=1; conNum<129; conNum*=2) {
 
         // Prints a message.
-        printf("#Cons=%d Started.\n",conNum);
+        printf("#Cons=%d Started. ",conNum);
 
-        // jobsInCounter and jobsOutCounter begin from -1 each time.
-        jobsInCounter = -1;
-        jobsOutCounter = -1;
+        //! Initializes variables.
+        // jobsLostCounter, jobsInCounter and jobsOutCounter quit begin from 0 each time.
+        jobsLostCounter = 0;
+        jobsInCounter = 0;
+        jobsOutCounter = 0;
+
+        // Flag begins as false.
+        quit = 0;
+
+        // Initializes Queue.
+        Queue *fifo;
+        fifo = queueInit(QUEUE_SIZE);
+        if (fifo == NULL) {
+            fprintf(stderr, "main: Queue Init failed.\n");
+            exit(1);
+        }
 
         //! Allocates memory for statistics with cells equal to the expected production.
         // tJobWait: Time taken from the moment a job is pushed to the queue until it gets popped.
@@ -122,14 +139,6 @@ int main () {
         else
             tDrift = (int *)malloc(totalJobs * sizeof(int));
 
-        // Initializes Queue.
-        Queue *fifo;
-        fifo = queueInit(QUEUE_SIZE);
-        if (fifo ==  NULL) {
-            fprintf (stderr, "main: Queue Init failed.\n");
-            exit (1);
-        }
-
         // Initializes consumer threads arguments.
         ConsumerArguments *consArgs = (ConsumerArguments *)malloc(sizeof(ConsumerArguments));
         consArgs->queue = fifo;
@@ -139,12 +148,13 @@ int main () {
         consArgs->tJobDur = tJobDur;
         consArgs->tMut = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
         pthread_mutex_init(consArgs->tMut, NULL);
+
         // Creates consumer threads.
         pthread_t con[conNum];
         for (int i=0; i<conNum; i++)
             pthread_create(&con[i], NULL, consumer, consArgs);
 
-        // Creates timer.
+        // Creates timer(s).
         Timer *timer;
         pthread_mutex_t *tMut = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
         pthread_mutex_init(tMut, NULL);
@@ -179,14 +189,11 @@ int main () {
             start(&timer[2]);
         }
 
-        // Substracts the lost jobs from the total number of expected production.
-        totalJobs -= jobsLost;
+        // Main waits for timer(s) to finish.
+        sleep(SECONDS_TO_RUN + 1);
 
-        // Waits for jobs to finish execution.
-        while(jobsOutCounter != totalJobs - 1);
-
-        // Waits for consumer threads to wait on notEmpty queue.
-        sleep(0.1);
+        // Sets flag to 1 for the consumers to exit after they are "tricked" that queue is not empty.
+        quit = 1;
 
         // Signals the consumer that queue is not empty, so they can quit safely.
         pthread_cond_broadcast(fifo->notEmpty);
@@ -195,18 +202,21 @@ int main () {
         for (int i=0; i<conNum; i++)
             pthread_join(con[i], NULL);
 
+        // Prints a message.
+        printf("JobsLost=%d.\n", jobsLostCounter);
+
         //! Saves statistics. The number of row represents the number of consumers of the test.
-        saveT(totalJobs, fTJobWait, tJobWait);
-        saveT(totalJobs, fTJobIn, tJobIn);
-        saveT(totalJobs, fTJobOut, tJobOut);
-        saveT(totalJobs, fTJobDur, tJobDur);
+        saveT(jobsOutCounter, fTJobWait, tJobWait);
+        saveT(jobsOutCounter, fTJobIn, tJobIn);
+        saveT(jobsOutCounter, fTJobOut, tJobOut);
+        saveT(jobsOutCounter, fTJobDur, tJobDur);
         if (mode == 4) {
             saveT(SECONDS_TO_RUN*1e3/period[0]-1, fTDrift0, tDrift0);
             saveT(SECONDS_TO_RUN*1e3/period[1]-1, fTDrift1, tDrift1);
             saveT(SECONDS_TO_RUN*1e3/period[2]-1, fTDrift2, tDrift2);
         }
         else
-            saveT(totalJobs-1, fTDrift, tDrift);
+            saveT(jobsOutCounter-1, fTDrift, tDrift);
 
         //! Cleans up.
         free(tJobWait);
@@ -265,7 +275,7 @@ void *work(void *arg) {
 }
 
 void *error() {
-    jobsLost++;
+    jobsLostCounter++;
 }
 
 void *producer(void *arg) {
@@ -296,7 +306,7 @@ void *producer(void *arg) {
         // Queue is full, so job is lost and errorFcn is executed.
         if (timer->queue->full) {
             //printf ("producer: queue FULL.\n");
-
+            timer->errorFcn();
             // Critical section ends.
             pthread_mutex_unlock(timer->queue->mut);
 
@@ -304,12 +314,12 @@ void *producer(void *arg) {
             pthread_cond_signal(timer->queue->notEmpty);
 
             // Critical section to run errorFcn starts.
-            pthread_mutex_lock(timer->tMut);
+            //pthread_mutex_lock(timer->tMut);
 
-            timer->errorFcn();
+
 
             // Critical section to run errorFcn ends.
-            pthread_mutex_unlock(timer->tMut);
+            //pthread_mutex_unlock(timer->tMut);
         }
         // Queue is not full, so the job is added to the queue.
         else {
@@ -328,7 +338,7 @@ void *producer(void *arg) {
 
             // Calculates tJobIn.
             int tJobIn = (tJobInEnd.tv_sec-tJobInStart.tv_sec)*1e6 + tJobInEnd.tv_usec-tJobInStart.tv_usec;
-            timer->tJobIn[++jobsInCounter] = tJobIn;
+            timer->tJobIn[jobsInCounter++] = tJobIn;
 
             // Critical section to write shared time statistics ends.
             pthread_mutex_unlock(timer->tMut);
@@ -362,19 +372,15 @@ void *consumer(void *arg) {
         while (consArgs->queue->empty) {
             //printf ("consumer: Queue EMPTY.\n");
             pthread_cond_wait(consArgs->queue->notEmpty, consArgs->queue->mut);
-            if(jobsOutCounter == consArgs->totalJobs - jobsLost - 1)
-                break;
-        }
-
-        // Checks if the number of consumed elements has matched the production. If yes, then this consumer exits.
-        if (jobsOutCounter == consArgs->totalJobs - jobsLost - 1) {
-            pthread_mutex_unlock(consArgs->queue->mut);
-            break;
+            if (quit) {
+                pthread_mutex_unlock(consArgs->queue->mut);
+                goto quit;
+            }
         }
 
         // Pops job from queue.
         gettimeofday(&tJobOutStart, NULL);
-        queueDel (consArgs->queue, &out);
+        queueDel(consArgs->queue, &out);
         gettimeofday(&tJobOutEnd, NULL);
 
         // Critical section ends.
@@ -391,8 +397,6 @@ void *consumer(void *arg) {
         // Critical section to write shared time statistics starts.
         pthread_mutex_lock(consArgs->tMut);
 
-        jobsOutCounter++;
-
         // Calculates tJobWait.
         int tJobWait = (tJobOutEnd.tv_sec-out.tJobWaitStart.tv_sec)*1e6 + tJobOutEnd.tv_usec-out.tJobWaitStart.tv_usec;
         consArgs->tJobWait[jobsOutCounter] = tJobWait;
@@ -403,10 +407,13 @@ void *consumer(void *arg) {
         int tJobDur = (tJobDurEnd.tv_sec-tJobDurStart.tv_sec)*1e6 + tJobDurEnd.tv_usec-tJobDurStart.tv_usec;
         consArgs->tJobDur[jobsOutCounter] = tJobDur;
 
+        jobsOutCounter++;
+
         // Critical section to write shared time statistics ends.
         pthread_mutex_unlock(consArgs->tMut);
     }
 
+    quit:
     return(NULL);
 }
 
